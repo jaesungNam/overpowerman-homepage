c76392aa50e261218302f150d2ef293619ac3add
Jaesung Nam <overpowerman@gmail.com>
2025-03-23 13:56:01 +0900

    i18n 파서 추가

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,20 @@
+import { headers } from 'next/headers';
+import acceptLanguageParser from 'accept-language-parser';
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  const locale = 'ko';
+  const headerStore = await headers();
+  const acceptLanguageHeader = headerStore.get('Accept-Language');
+
+  let locale = 'ko';
+  if (acceptLanguageHeader) {
+    const acceptLanguage = acceptLanguageParser.parse(acceptLanguageHeader)?.[0];
+    if (acceptLanguage?.code) {
+      locale = acceptLanguage?.code;
+    }
+  }
 
   return {
     locale,
