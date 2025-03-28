389ad2bc3297bd03254eadf3d4fe16088f7309c1
Jaesung Nam <overpowerman@gmail.com>
2025-03-29 07:17:48 +0900

    tinymce 추가

--- src/app/tinymce/page.tsx
+++ src/app/tinymce/page.tsx
@@ -1,4 +1,12 @@
-import TinymcePage from '@/components/TinymcePage';
+'use client';
+
+import dynamic from 'next/dynamic';
+import { Loader, LoadingOverlay } from '@mantine/core';
+
+const TinymcePage = dynamic(() => import('@/components/TinymcePage'), {
+  loading: () => <Loader />,
+  ssr: false,
+});
 
 const Page = () => {
   return <TinymcePage />;
